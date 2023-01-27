7146e63a3501f020dc64a554882b5ae482f52961
BradockBr <robtognetti@gmail.com>
2023-01-26 21:05:31 -0300

    req 1

---
+++ src/migrations/20230126235239-categories.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('categories');
+  }
+};
